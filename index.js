ecdc75954fef0731a132e7c9e6033c6a31f95cba
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-05-28 19:15:54 -0400

    2 files modified

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const members = require('./members');
 const path = require('path');
 const express = require('express');
-const moment = require('moment')
-const logger = require('./middleware/logger')
+const logger = require('./middleware/logger');
 const port = 3010;
 const app = express();
 
 // Create middleware
-
 
 // Init middleware
 app.use(logger);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/members', (req, res) => {
-  res.json(members);
-});
+app.get('/api/members', (req, res) => res.json(members));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 const logger = (req, res, next) => {
   console.log(`${req.protocol}://${req.get('host')}: ${moment().format()}`);
   next();
