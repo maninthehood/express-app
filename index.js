6ac25efaa068175b7f1a7f239d52c28bd22111a7
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-05-28 16:56:37 -0400

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const app = express();
-const port = 3010;
+const members = require('./members');
 const path = require('path');
+const express = require('express');
+const port = 3010;
+const app = express();
+
+// Create middlewear
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get('host')}`);
+  next();
+};
+
+// Init middlewear
+app.use(logger);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ members.js
@@ -0,0 +1,20 @@
+const members = [
+    {
+      id: 1,
+      name: 'John Doe',
+      status: 'active',
+    },
+    {
+      id: 1,
+      name: 'ejn alam',
+      status: 'active',
+    },
+    {
+      id: 1,
+      name: 'John Doe',
+      status: 'active',
+    },
+  ];
+
+
+module.exports = members
