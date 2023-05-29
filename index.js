3d120a210ab8bb0d9298bacc6d703bf8ad9cedcf
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-05-28 20:21:55 -0400

    1 file modified

--- index.js
+++ index.js
@@ -13,17 +13,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/members', (req, res) => res.json(members));
-
-app.get('/api/members:id', (req, res) =>{
-  const found = members.some(member => member.id === parseInt(req.params.id))
-  if (found){
-    res.json(members.filter(member => member.id === parseInt(req.params.id)))
-  } else {
-    res.status(400).json({msg: "Member not found"})
-  }
-
-})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
