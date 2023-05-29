7bc91776cb28f0160af956be0f73ac6efe4e452f
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-05-28 20:04:18 -0400

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-const members = require('./members');
 const path = require('path');
 const express = require('express');
 const logger = require('./middleware/logger');
 const port = 3010;
 const app = express();
 
-// Create middleware
-
-// Init middleware
 // app.use(logger);
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/members', (req, res) => res.json(members));
+app.use('/api/members',require("./routes/api/members"))
 
-app.get('/api/members:id', (req, res) =>{
-  const found = members.some(member => member.id === parseInt(req.params.id))
-  if (found){
-    res.json(members.filter(member => member.id === parseInt(req.params.id)))
-  } else {
-    res.status(400).json({msg: "Member not found"})
-  }
-
-})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
---
+++ routes/api/members.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const members = require('./members');
+
+// Init middleware
+
+router.get('/api/members', (req, res) => res.json(members));
+
+router.get('/api/members:id', (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: 'Member not found' });
+  }
+});
+
+module.exports = router
